989cb444766dbd17e7e1e36a0359af8731b944ae
Sara Ouzande <ouzandeitu@proton.me>
2025-03-03 22:57:20 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 // Conexión a MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb+srv://ouzandeitu@proton.me:Saraouzande96@cluster0.fkomt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,  // Evitar advertencias de índice
+      useFindAndModify: false,  // Evitar advertencias de findAndModify
     });
     console.log('Conexión exitosa a la base de datos');
   } catch (error) {
-    console.error('Error al conectar a la base de datos', error);
-    process.exit(1);
+    console.error('Error al conectar a la base de datos:', error.message);
+    process.exit(1);  // Termina el proceso si no se puede conectar
   }
 };
 
